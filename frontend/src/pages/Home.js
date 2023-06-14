82c1b7f1fdd3232dd74a6e9f785fb962c7a8a1c8
sajithmadusamkha <sajithmadusankha02@gmail.com>
2023-06-14 10:47:03 +0530

    content updated

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,27 @@
+import { useEffect, useState } from "react";
+
 const Home = () => {
+  const [workouts, setWorkout] = useState(null);
+
+  useEffect(() => {
+    const fetchWorkouts = async () => {
+      const response = await fetch("/api/workouts");
+      const json = await response.json();
+
+      if (response.ok) {
+        setWorkout(json);
+      }
+    };
+
+    fetchWorkouts();
+  }, []);
+
   return (
-    <div>
-      <h2>Home</h2>
+    <div className="home">
+      <div className="workouts">
+        {workouts &&
+          workouts.map((workout) => <p key={workout._id}>{workout.title}</p>)}
+      </div>
     </div>
   );
 };
