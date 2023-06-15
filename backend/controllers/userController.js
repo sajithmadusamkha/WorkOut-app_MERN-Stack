0929c4b6b67cf0ef1cfab48a861681ee14546764
sajithmadusamkha <sajithmadusankha02@gmail.com>
2023-06-15 11:30:15 +0530

    email and passowrd validation

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -13,7 +13,6 @@ const signupUser = async (req, res) => {
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
-  res.json({ mssg: "signup user" });
 };
 
 module.exports = { loginUser, signupUser };
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const bcrypt = require("bcrypt");
+const validator = require("validator");
 
 const userSchema = new Schema({
   email: {
@@ -15,6 +16,18 @@ const userSchema = new Schema({
 });
 
 userSchema.statics.signup = async function (email, password) {
+  if (!email || !password) {
+    throw Error("All fields must be filled");
+  }
+
+  if (!validator.isEmail(email)) {
+    throw Error("Email is not valid");
+  }
+
+  if (!validator.isStrongPassword(password)) {
+    throw Error("Password is not strong enough");
+  }
+
   const exist = await this.findOne({ email });
 
   if (exist) {
