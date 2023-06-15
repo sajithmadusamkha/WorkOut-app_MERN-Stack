3c727b3edc521ee116e934fcf223ce5416a9f135
sajithmadusamkha <sajithmadusankha02@gmail.com>
2023-06-15 12:31:27 +0530

    add jwt authentication

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,9 @@
 const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
+
+const createToken = (_id) => {
+  return jwt.sign({ _id }, process.env.SECRET, { expiresIn: "3d" });
+};
 
 const loginUser = async (req, res) => {
   res.json({ mssg: "login user" });
@@ -9,7 +14,10 @@ const signupUser = async (req, res) => {
 
   try {
     const user = await User.signup(email, password);
-    res.status(200).json({ email, user });
+
+    const token = createToken(user._id);
+
+    res.status(200).json({ email, token });
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
